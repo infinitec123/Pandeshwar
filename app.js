3f8e6dc65e337f1a8fb8b3d778692271b76ffe90
infinitec123 <infinitecsolutions@gmail.com>
2013-10-17 20:04:50 +0530

    online_offline

--- app.js
+++ app.js
@@ -6,8 +6,8 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var fs          = require('fs');
 var routes = require('./routes/index.js');
-
 
 var app = express();
 
@@ -17,9 +17,10 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
+app.use(express.limit('1mb'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(app.router);
+//app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
@@ -27,9 +28,16 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/contact', routes.contact);
-app.post('/mailsend', routes.mailwrite);
+// Import the routes
+fs.readdirSync('routes').forEach(function(file) {
+  if ( file[0] == '.' ) return;
+  var routeName = file.substr(0, file.indexOf('.'));
+  require('./routes/' + routeName)(app);
+});
+
+//app.get('/', routes.index);
+//app.get('/contact', routes.contact);
+//app.post('/mailsend', routes.mailwrite);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,24 @@
+module.exports = function(app, models) {
 
+var is_pandeshwar_logged_in = false;  
 
+var check_LoginStatus = function(){
+  var chat_line = "Pandeshwar is Offline";
+  
+  if(is_pandeshwar_logged_in){
+    chat_line = "Pandeshwar is Online";
+  }
+  return chat_line
+}
 
-
-exports.index = function(req, res){
-  //res.render('index', { title: 'Express' });
+app.get('/', function (req, res) {
   res.sendfile('views/index.html');
-};
+});
 
-exports.contact = function(req, res){
-  //res.sendfile('views/contact.html');
-  res.render('contact', {title: 'Chat with Pandeshwar'});
-};
+app.get('/contact', function (req, res) {
+  res.render('contact', {chatstatus: check_LoginStatus()});
+});
+
 
 var writeEMail = function(name, email, message, callback) {
 
@@ -34,6 +42,77 @@ var writeEMail = function(name, email, message, callback) {
         });
 }; 
 
+app.post('/mailsend', function (req, res) {
+  var name = req.param('name', '');
+    var message = req.param('message', '');
+    var email = req.param('email', null);
+    //console.log("Post request came from" + email);
+    writeEMail(name, email, message, function(success){
+      if (success) {
+        res.send('success');
+      } else {
+        res.send('failure');
+      }
+});
+});
+
+
+app.get('/login', function(req, res){
+ res.render('login');
+});
+
+app.get('/logout', function(req, res){
+ res.render('logout');
+});
+
+app.post('/login', function(req, response){
+ var email = req.body.email;
+ var password = req.body.password;
+ console.log('Request received to login received with email: ' + email + ' Password: ' + password);
+ if(email == "raosharat@gmail.com" && password =="Tenacity456!"){
+  console.log("Welcome Pandeshwar. You are authorized.");
+  is_pandeshwar_logged_in = true; 
+    //res.render('contact', {chatstatus: check_LoginStatus()});
+    response.writeHead(301,
+     {Location: '/contact'}
+    );
+    response.end();
+
+ } else {res.send(404);}
+ 
+});
+
+app.post('/logout', function(req, response){
+ var email = req.body.email;
+ var password = req.body.password;
+ console.log('Request received to logout with email: ' + email + ' Password: ' + password);
+ if(email == "raosharat@gmail.com" && password =="Tenacity456!"){
+  console.log("GoodBye Pandeshwar. You are logged out.");
+  is_pandeshwar_logged_in = false; 
+    //res.render('contact', {chatstatus: check_LoginStatus()});
+    response.writeHead(301,
+     {Location: '/contact'}
+    );
+    response.end();
+
+ } else {res.send(404);}
+ 
+});
+
+}    
+
+
+
+/* exports.index = function(req, res){
+  //res.render('index', { title: 'Express' });
+  res.sendfile('views/index.html');
+};
+
+exports.contact = function(req, res){
+  //res.sendfile('views/contact.html');
+  res.render('contact', {title: 'Chat with Pandeshwar'});
+}; 
+
 
 exports.mailwrite = function (req, res) {
     var name = req.param('name', '');
@@ -49,6 +128,5 @@ exports.mailwrite = function (req, res) {
     }); 
 
    /* */
-};
 
 
