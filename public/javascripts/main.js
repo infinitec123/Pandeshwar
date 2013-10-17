3aca865be9de5a95568261318206664f7cee9947
infinitec123 <infinitecsolutions@gmail.com>
2013-10-17 17:52:53 +0530

    refining

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,7 +4,9 @@
 			var $name_sender = $('#name');
 			var $email_sender = $('#email');
 			var $message= $('#message');
+			var $formsubmit_button = $('#contact-submit');
 			
+
 			function validateEmail(email) { 
    				 var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     				return re.test(email);
@@ -17,7 +19,7 @@
 				$message.val('');
 			}
 
-			$ContactForm.submit(function(e){
+			$formsubmit_button.click(function(e){
 				e.preventDefault();
 				if($name_sender.val() == "" || !validateEmail($email_sender.val()) || $message.val() ==""){
 					alert("Fill the form completely.");
