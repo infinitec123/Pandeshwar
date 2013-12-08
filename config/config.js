670738b75d221bcaba9706b996e1d673b09ef14a
infinitec123 <infinitecsolutions@gmail.com>
2013-12-08 10:14:31 +0530

    config

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ module.exports = {
 		facebook: {
 			clientID: "587869427929487",
 			clientSecret: "86a7f894441c1d939dd798a809bd295b",
-			callbackURL: "http://localhost:3000/auth/facebook/callback"
+			callbackURL: "http://pandeshwar.herokuapp.com/auth/facebook/callback"
 		}
 	},
   	production: {
