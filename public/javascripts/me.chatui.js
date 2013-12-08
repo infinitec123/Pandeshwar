73d4799f6434f60b0d00f08d9c6a3b8e51b3a51a
infinitec123 <infinitecsolutions@gmail.com>
2013-12-08 16:45:03 +0530

    test

--- public/javascripts/me.chatui.js
+++ public/javascripts/me.chatui.js
@@ -159,7 +159,9 @@ me.chatui = (function(){
   onLogin = function(){
     setSliderPosition('opened');
     jqueryMap.$welcome_body.html('<button id="log_out" type="submit" class="btn btn-info">Logout</button>');
-    jqueryMap.$logout_button.bind('utap', LogMeout);
+    $jqueryMap.$logout_button = $('#log_out');
+    console.log($jqueryMap.$logout_button);
+    $('#log_out').bind('click', LogMeout);
   };
 
   onLogout = function(){
